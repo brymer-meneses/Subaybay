77891798f5fc62cf19bf04edcf85831aa02866c7
embryzon <95128949+embryzon@users.noreply.github.com>
2024-03-08 16:45:21 +0800

    better landing page

---
+++ src/routes/dashboard/[requestID]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import * as d3 from "d3";
+  var data = [30, 86, 168, 281, 303, 109];
+
+  let el;
+
+  onMount(() => {
+    d3.select(el)
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .style("width", function (d) {
+        return d + "px";
+      })
+      .text(function (d) {
+        return d;
+      });
+  });
+</script>
+
+<div bind:this={el} class="chart"></div>
+
+<style>
+  .chart :global(div) {
+    font: 10px sans-serif;
+    background-color: steelblue;
+    text-align: right;
+    padding: 3px;
+    margin: 1px;
+    color: white;
+  }
+</style>
