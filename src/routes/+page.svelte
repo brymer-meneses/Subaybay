4efd71f839b1e2f634c1a609482fec30eee196b5
embryzon <95128949+embryzon@users.noreply.github.com>
2024-03-08 22:25:38 +0800

    minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-row items-center justify-center gap-20 w-screen h-screen">
+<div class="flex flex-row items-center justify-center gap-40 w-screen h-screen">
   <div class="relative">
     <img src="UP.png" alt="up-logo" class="w-[500px] absolute z-10" />
     <div
@@ -6,11 +6,11 @@
     />
   </div>
 
-  <div class="flex flex-col gap-4 z-20">
+  <div class="flex flex-col gap-4 z-10">
     <h1 class="text-8xl font-extrabold text-maroon">Subaybay</h1>
     <h2 class="text-2xl">Request Monitoring System</h2>
     <button
-      class="bg-white text-black py-4 text-xl border border-maroon rounded-2xl hover:bg-maroon hover:text-white"
+      class="bg-white text-maroon py-4 text-xl border border-maroon rounded-2xl hover:bg-maroon hover:text-white"
       >Continue with google temp button lma</button
     >
   </div>
