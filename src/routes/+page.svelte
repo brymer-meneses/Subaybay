d37fb067686c5a31893ffec7a1ac12a07e36154f
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-03-08 16:49:38 +0800

    start landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<script lang="ts">
-</script>
+<div class="flex flex-row items-center justify-center gap-20 w-screen h-screen">
+  <div class="relative">
+    <img src="UP.png" alt="up-logo" class="w-[500px] absolute z-10" />
+    <div
+      class="rounded-full bg-red-500 blur-[200px] aspect-square w-[400px] z-0"
+    />
+  </div>
+
+  <div class="flex flex-col gap-4">
+    <h1 class="text-8xl font-bold">Subaybay</h1>
+    <h2 class="text-4xl">Request Monitoring System</h2>
+  </div>
+</div>
