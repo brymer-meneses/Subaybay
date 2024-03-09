d15b874ec1dbecfffbc4064eb5cf6a3e93d05b31
arktii <72131971+Arktii@users.noreply.github.com>
2024-03-09 15:55:32 +0800

    add parallel graph thing

--- src/routes/flowConfig/+page.svelte
+++ src/routes/flowConfig/+page.svelte
@@ -14,7 +14,6 @@
 	let graphHeight : number;
 
     let linearStageCount : number = 6; // how to make automatic?
-    let points : Point[] = []
 
 	const margin = {
 		top: 20,
@@ -35,6 +34,7 @@
         // actually, it's gonna start as just the initial leading to the end then it mutates as 
         // the user adds nodes
 
+    // this stores the connection between stages
     const flow: { [key: string]: string[]} = {};
     flow["initial"] = ["staff 1"];
     flow["staff 1"] = ["staff 2", "staff 3"];
@@ -44,12 +44,15 @@
     flow["staff 3A"] = ["UR"];
     flow["UR"] = ["final"];
 
+    // todo: look into reducing the amount of data structures here
+    // this links a stage to its point
+    let stageToPoint : {[key: string]: Point} = {};
+
     let layers : string[][];
 
     onMount(() => {
         recompute();
         window.addEventListener('resize', recompute);
-        //todo add event listener for particular button clicks
     });
     
     // Todo: Clean Up
@@ -63,30 +66,13 @@
         lineEnd = graphWidth - margin.right;
         xSpacing = (lineEnd - lineStart) / (linearStageCount - 1);
 
-        //todo for progress view, include data about progress per point (color and stuff?)
-        points = [];
+        stageToPoint = {};
 
         resetLayers();
 
         readFlow("initial", 0);
 
-        // addPoint(lineStart, center.y);
-
         createPoints();
-
-        // readFlow();
-
-        // addPoint(lineEnd, center.y);
-
-        // let x1 = lineStart;
-        // let x2 = lineStart + xSpacing;
-        // for(let i = 2; i <= linearStageCount + 1; i++) {
-        //     let point : Point = { x : x1, y : center.y, pos : 0};
-        //     points.push(point);
-
-        //     x1 = x2;
-        //     x2 = lineStart + (i * xSpacing);
-        // }
     }
 
     function resetLayers() {
@@ -118,22 +104,21 @@
         //todo find a way to ensure order is preserved
         for(let pos = 0; pos < linearStageCount; pos++) {
             let layer = layers[pos];
-            console.log(pos + " " + layer.length);
             const x = lineStart + pos * xSpacing
             const y0 = center.y - (((layer.length - 1) / 2) * ySpacing); //trust me
 
             for(let i = 0; i < layer.length; i++) {
                 const y = y0 + (i * ySpacing);
-                addPoint(x, y);
-
+                addPoint(x, y, layer[i]);
             }
             
+            //todo do something with first pos = 0 and last pos = layers.Count - 1
         }
     }
 
-    function addPoint(x : number, y : number) {
+    function addPoint(x : number, y : number, stage : string) {
         const point : Point = { x : x, y : y};
-            points.push(point);
+        stageToPoint[stage] = point;
     }
 
 </script>
@@ -142,22 +127,26 @@
     <div class="border-2 border-black justify-center w-[100%] max-w-[800px] h-[500px]">
         <svg id="graph" viewBox="0 0 {graphWidth} {graphHeight}" width=100% bind:this={graph}>
             <g>
-                <!-- {#each points as point, index}
-                    {#if index < points.length - 1}
-                        <NodeLine x1={point.x} y1={point.y} x2={points[index + 1].x} y2={points[index + 1].y}></NodeLine>
-                    {/if}
-                {/each} -->
+                {#if Object.keys(stageToPoint).length > 0} 
+                    {#each Object.keys(flow) as start}
+                        {#each flow[start] as end}
+                            <NodeLine 
+                                x1={stageToPoint[start].x} 
+                                y1={stageToPoint[start].y} 
+                                x2={stageToPoint[end].x} 
+                                y2={stageToPoint[end].y}
+                            ></NodeLine>
+                        {/each}
+                    {/each}
+                {/if}
             </g>
             
             <g>
-                {#each points as point, index}
-                    <!-- todo do something special with the start and end -->
-                    {#if index == 0 || index == points.length - 1}
-                        <NodeButton cx={point.x} cy={point.y} r={radius} editable={false}></NodeButton>
-                    {:else}
+                {#if Object.keys(stageToPoint).length > 0}
+                    {#each Object.values(stageToPoint) as point}
                         <NodeButton cx={point.x} cy={point.y} r={radius}></NodeButton>
-                    {/if}
-                {/each}
+                    {/each}
+                {/if}
             </g>
         </svg>
     </div>  
