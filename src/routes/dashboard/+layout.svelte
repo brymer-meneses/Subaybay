a16d5a5c13665ab69eacbde7cee1fa1a95e94bd4
embryzon <95128949+embryzon@users.noreply.github.com>
2024-03-09 22:50:05 +0800

    recreated sidebar but not rspnsv yet and w/o icons

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -5,37 +5,62 @@
   import UP from "$lib/assets/UP.png";
 </script>
 
-<div class="fixed bg-up_maroon h-dvh w-20">
+<div class="fixed bg-up_maroon h-dvh w-20 flex flex-col justify-between">
   <!-- UP logo container -->
-  <div class="flex items-center justify-center border-b">
-    <img src={UP} alt="UP logo" class="h- aspect-square rounded-full m-2" />
+  <div class="flex items-center justify-center border-b h-20">
+    <div>
+      <img src={UP} alt="UP logo" class="aspect-square p-3" />
+    </div>
   </div>
-  <!-- icons container -->
-  <div class="flex flex-col h-full">
-    <!-- replace with icons -->
-    <img
-      src="https://placehold.co/50"
-      alt="UP logo"
-      class="h-20 aspect-square rounded-full"
-    />
-    <img
-      src="https://placehold.co/50"
-      alt="UP logo"
-      class="h-20 aspect-square rounded-full"
-    />
-    <img
-      src="https://placehold.co/50"
-      alt="UP logo"
-      class="h-20 aspect-square rounded-full text-white"
-    />
+  <!-- nav icons here -->
+  <div class=" flex-grow flex flex-col gap-2 items-center space-y-4">
+    <div class="relative group mt-4">
+      <span
+        class="absolute top-1/4 left-20 text-white bg-gray-600 px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity"
+        >Admin</span
+      >
+      <img
+        src="https://placehold.co/50"
+        alt="Log out"
+        class="aspect-square m4 rounded-full"
+      />
+    </div>
+    <div class="relative group">
+      <span
+        class="absolute top-1/4 left-20 text-white bg-gray-600 px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity"
+        >Staff Dashboard</span
+      >
+      <img
+        src="https://placehold.co/50"
+        alt="Log out"
+        class="aspect-square m4 rounded-full"
+      />
+    </div>
+    <div class="relative group">
+      <span
+        class="absolute top-1/4 left-20 text-white bg-gray-600 px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity"
+        >User Settings</span
+      >
+      <img
+        src="https://placehold.co/50"
+        alt="Log out"
+        class="aspect-square m4 rounded-full"
+      />
+    </div>
   </div>
   <!-- logout  -->
-  <div class="absolute bottom-0 border-t">
-    <img
-      src="https://placehold.co/50"
-      alt="UP logo"
-      class="h-20 aspect-square rounded-full"
-    />
+  <div class="border-t h-20 flex items-center justify-center">
+    <div class="relative group">
+      <span
+        class="absolute top-1/4 left-20 text-white bg-gray-600 px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition-opacity"
+        >Logout</span
+      >
+      <img
+        src="https://placehold.co/50"
+        alt="Log out"
+        class="aspect-square m4 rounded-full"
+      />
+    </div>
   </div>
 </div>
 <div class="ml-20">
