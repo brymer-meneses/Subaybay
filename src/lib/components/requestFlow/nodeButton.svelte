53e26d7f66fd5abca1a97f873e16e88ee1f22e97
arktii <72131971+Arktii@users.noreply.github.com>
2024-03-09 12:21:19 +0800

    add responsive lines

---
+++ src/lib/components/requestFlow/nodeButton.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+	export let cx : number = 0;
+	export let cy : number = 0;
+	export let r : number = 40;
+	export let editable : boolean = true;
+	
+	let color = editable ? 'red' : 'blue';
+	
+	function handleMouseOver(e : any) {
+		if(!editable) return;
+		//show icon that allows editing
+		color = 'green';
+	}
+	function handleMouseOut(e : any) {
+		if(!editable) return;
+
+		color = 'red';
+	}
+
+	// on click, thingy thing
+</script>
+
+<circle cx={cx} cy={cy} r={r} fill={color}
+	on:mouseover={handleMouseOver} on:mouseout={handleMouseOut} />
