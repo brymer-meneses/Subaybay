bb2f7b626bc70b5e30153d36f99768c5b9311a67
arktii <72131971+Arktii@users.noreply.github.com>
2024-03-09 12:16:25 +0800

    add responsive line

---
+++ src/lib/components/requestFlow/nodeLine.svelte
@@ -0,0 +1,58 @@
+<script lang='ts'>
+    import { onMount } from "svelte";
+
+	export let x1 : number = 0, y1 : number = 100;
+	export let x2 : number = 500, y2 : number = 100;
+    $: slope = (y2 - y1) / (x2 - x1);
+    // todo export a callback?    
+
+    let line : any;
+    let addButton : any;
+    
+    let hovered : boolean = false;
+    let hoveredX : number;
+    let hoveredY : number;
+	
+	let color = 'black';
+    let thickness = 3;
+
+    onMount(() => {
+        window.addEventListener('mousemove', handleMouseMove);
+    });
+
+    function handleMouseMove(e: any) {
+        if(!hovered) return;
+
+        hoveredX = e.clientX - line.getBoundingClientRect().left + x1;
+        hoveredY = slope * (hoveredX - x1) + y1;
+    }
+	
+	function handleMouseOver(e : any) {
+        hovered = true;
+
+		color = 'green';
+        thickness = 6;
+	}
+
+	function handleMouseOut(e : any) {
+        hovered = false;
+        color = 'black';
+        thickness = 3;
+	}    
+</script>
+
+<g>
+    <!-- hitbox -->
+    <line x1={x1} y1={y1} x2={x2} y2={y2} 
+        stroke=transparent stroke-width={40}
+        on:mouseover={handleMouseOver} on:mouseout={handleMouseOut}/>
+
+    <line bind:this={line} x1={x1} y1={y1} x2={x2} y2={y2}
+        stroke={color} stroke-width={thickness} 
+        style="pointer-events: none;"/>
+    
+    {#if hovered}
+        <circle bind:this={addButton} r={10} cx={hoveredX} cy={hoveredY} style="pointer-events: none;"></circle>
+        <!-- on click add new node -->
+    {/if}
+</g>
--- src/routes/flowConfig/+page.svelte
+++ src/routes/flowConfig/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+    import NodeButton from '$lib/components/requestFlow/nodeButton.svelte';
+    import NodeLine from '$lib/components/requestFlow/nodeLine.svelte';
     import {onMount} from 'svelte';
     import * as d3 from 'd3';
+
+    type Point = {
+        x : number;
+        y : number;
+    }
   
     let graph : any;
 
@@ -8,10 +15,11 @@
 	let graphHeight : number;
 
     let linearStageCount : number = 5; // ignores parallel stages
+    let points : Point[] = []
 
 	const margin = {
 		top: 20,
-		right: 20,
+		right: 30,
 		bottom: 30,
 		left: 30
 	};
@@ -23,68 +31,59 @@
         //todo add event listener for particular button clicks
     });
     
-    // Todo: Clean
+    // Todo: Clean Up
+        // How organize this 
     function redraw(): void {
-        d3.select(graph).html(null);
+        d3.select(graph);
 
         graphWidth = d3.select(graph).node().getBoundingClientRect().width;
 		graphHeight = d3.select(graph).node().getBoundingClientRect().height;
+        let g = d3.select(graph).append('g');
 
-        console.log(graphWidth + " " + graphHeight);
-        
         // pick best place to compute this stuff
         const center = { x : graphWidth / 2, y : graphHeight / 2};
-        const width = graphWidth - margin.left - margin.right;
-        const height = graphHeight - margin.top - margin.bottom;
 
         const lineStart = margin.left;
         const lineEnd = graphWidth - margin.right;
         const xSpacing = (lineEnd - lineStart) / (linearStageCount - 1);
 
-        let g = d3.select(graph)
-            .append('g')
+        //todo for progress view, include data about progress per point (color and stuff?)
+        points = [];
 
-        // Todo: Make into buttons
-            // edit for created nodes (+ popup that allows deletions)
-            // + for new nodes
-
-        // Draw
+        // Create points to be created later on
+        // Starting from second point
         let x1 = lineStart;
         let x2 = lineStart + xSpacing;
-        for(let i = 1; i <= linearStageCount; i++)
-        {
-            g.append('circle')
-            .attr('cx', x1)
-            .attr('cy', center.y)
-            .attr('r', radius)
-            .attr('fill', 'gray');
-            
-            g.append('line')
-            .attr('x1', x1).attr('y1', center.y)
-            .attr('x2', x2).attr('y2', center.y)
-            .attr('stroke', "blue")
-            .attr('stroke-width', 2)
+        for(let i = 2; i <= linearStageCount + 1; i++) {
+            let point : Point = { x : x1, y : center.y};
+            points.push(point);
 
             x1 = x2;
             x2 = lineStart + (i * xSpacing);
         }
-        
-        //?? Should first and last be specially rendered ?
-        // First and last should actually be untouchable, 
-        // since first is always initial (encode) and last is always Finish
-
-        // Draw End
-        g.append('circle')
-            .attr('cx', lineEnd - radius)
-            .attr('cy', center.y)
-            .attr('r', radius)
-            .attr('fill', 'gray')
     }
     
 </script>
 
 <div class="flex justify-center border-blue border-4">
     <div class="border-2 border-black justify-center w-[100%] max-w-[800px]">
-        <svg id="graph" viewBox="0 0 {graphWidth} {graphHeight}" width=100% bind:this={graph}></svg>
+        <svg id="graph" viewBox="0 0 {graphWidth} {graphHeight}" width=100% bind:this={graph}>
+            <g>
+                {#each points as point, index}
+                    {#if index < points.length - 1}
+                        <NodeLine x1={point.x} y1={point.y} x2={points[index + 1].x} y2={points[index + 1].y}></NodeLine>
+                    {/if}
+                {/each}
+            </g>
+            
+            {#each points as point, index}
+                <!-- todo do something special with the start and end -->
+                {#if index == 0 || index == points.length - 1}
+                    <NodeButton cx={point.x} cy={point.y} r={radius} editable={false}></NodeButton>
+                {:else}
+                    <NodeButton cx={point.x} cy={point.y} r={radius}></NodeButton>
+                {/if}
+            {/each}
+        </svg>
     </div>  
 </div>
