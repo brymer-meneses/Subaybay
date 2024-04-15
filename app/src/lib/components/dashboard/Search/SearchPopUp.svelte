407ad68b91314636cd1d1cfda7cdf627caf47ffd
embryzon <95128949+embryzon@users.noreply.github.com>
2024-04-16 00:57:41 +0800

    added absolute class to popup and removed the <p>

--- app/src/lib/components/dashboard/Search/SearchPopUp.svelte
+++ app/src/lib/components/dashboard/Search/SearchPopUp.svelte
@@ -18,46 +18,48 @@
   });
 </script>
 
-<p class="text-sm text-muted-foreground">
+<!-- <p class="text-sm text-muted-foreground">
   Press
   <kbd
     class="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100"
   >
     <span class="text-xs">⌘</span>J
   </kbd>
-</p>
-<Command.Dialog bind:open>
-  <Command.Input placeholder="Type a command or search..." />
-  <Command.List>
-    <Command.Empty>No results found.</Command.Empty>
-    <Command.Group heading="General">
-      <Command.Item>
-        <span>Inbox</span>
-      </Command.Item>
-      <Command.Item>
-        <span>Request Tracker</span>
-      </Command.Item>
-      <Command.Item>
-        <span>Logout</span>
-      </Command.Item>
-    </Command.Group>
-    <Command.Separator />
-    <Command.Group heading="Only admins should see the following">
-      <Command.Item>
-        <span>Registered accounts and Roles</span>
-        <Command.Shortcut>⌘P</Command.Shortcut>
-      </Command.Item>
-      <Command.Item>
-        <span>Request Configuration</span>
-        <Command.Shortcut>⌘B</Command.Shortcut>
-      </Command.Item>
-      <Command.Item>
-        <span>Settings</span>
-        <Command.Shortcut>⌘S</Command.Shortcut>
-      </Command.Item>
-    </Command.Group>
-  </Command.List>
-</Command.Dialog>
+</p> -->
+<div class="absolute">
+  <Command.Dialog bind:open>
+    <Command.Input placeholder="Type a command or search..." />
+    <Command.List>
+      <Command.Empty>No results found.</Command.Empty>
+      <Command.Group heading="General">
+        <Command.Item>
+          <span>Inbox</span>
+        </Command.Item>
+        <Command.Item>
+          <span>Request Tracker</span>
+        </Command.Item>
+        <Command.Item>
+          <span>Logout</span>
+        </Command.Item>
+      </Command.Group>
+      <Command.Separator />
+      <Command.Group heading="Only admins should see the following">
+        <Command.Item>
+          <span>Registered accounts and Roles</span>
+          <Command.Shortcut>⌘P</Command.Shortcut>
+        </Command.Item>
+        <Command.Item>
+          <span>Request Configuration</span>
+          <Command.Shortcut>⌘B</Command.Shortcut>
+        </Command.Item>
+        <Command.Item>
+          <span>Settings</span>
+          <Command.Shortcut>⌘S</Command.Shortcut>
+        </Command.Item>
+      </Command.Group>
+    </Command.List>
+  </Command.Dialog>
+</div>
 
 <style>
   span {
