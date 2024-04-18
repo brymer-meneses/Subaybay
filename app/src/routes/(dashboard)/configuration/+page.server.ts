72216698e47e7694dc44ed92225f078f36f45874
arktii <72131971+Arktii@users.noreply.github.com>
2024-04-18 18:00:31 +0800

    Squashed commit of the following:
    commit 44164547edf3952ecee33df4629c5be57b711962
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Thu Apr 18 17:56:23 2024 +0800
    
        fix bug with loading user profile pics
    
    commit 6e5785a5c9c2d7e4ce3d0c0cd3490835e782f19a
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Thu Apr 18 17:46:18 2024 +0800
    
        Use database users in configuration
    
    commit 330dae6e7e1a7bc7fec5834365aa8dd81ddee0ad
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Thu Apr 18 16:32:54 2024 +0800
    
        Make minor database changes
    
    commit 1420afeb107b289c22233ad2154fc23ef558a9b1
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Thu Apr 18 12:37:17 2024 +0800
    
        Remove unnecessary files and code from config
    
    commit 64cd8ac114cd730940da9558e0c6182d8da029ae
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Thu Apr 18 12:35:11 2024 +0800
    
        Improve configuration ui
    
    commit 2ce12a3d3d29c95d30bec94884e5515dfc3e49fc
    Merge: bd7e3b2 0b8ffc5
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Thu Apr 18 11:02:05 2024 +0800
    
        Merge branch 'main' into document-config
    
    commit bd7e3b247ad0b5e8681265c6e37cf9afc07d1483
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Wed Apr 17 23:25:26 2024 +0800
    
        Improve configuration ui
    
        Also fixed bug where array was not properly updating leading to weird deletion behavior
    
    commit 695a8c1589e8eaa044b850d29ef00e8de9d85686
    Merge: 084e299 f36193f
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Wed Apr 17 22:22:12 2024 +0800
    
        Merge remote-tracking branch 'origin/main' into document-config
    
    commit 084e29985017fc4464861a5db09ac4c464342808
    Author: arktii <72131971+Arktii@users.noreply.github.com>
    Date:   Wed Apr 17 22:11:11 2024 +0800
    
        Start switching to shadcn

--- app/src/routes/(dashboard)/configuration/+page.server.ts
+++ app/src/routes/(dashboard)/configuration/+page.server.ts
@@ -1,10 +1,17 @@
 import type { PageServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
+import { user } from "$lib/server/database";
 
 export const load: PageServerLoad = async (event) => {
   if (!event.locals.user) {
     redirect(302, "/auth/login");
   }
 
-  return { userInfo: event.locals.user };
+  let cursor = user.find();
+  let allUsers: any = [];
+  for await (const doc of cursor) {
+    allUsers.push({email : doc.email, name : doc.name, profileUrl : doc.profileUrl})
+  }
+
+  return { userInfo: event.locals.user, allUsers: allUsers };
 }
--- mongo-init.js
+++ mongo-init.js
@@ -1,7 +1,8 @@
 // https://www.mongodb.com/docs/mongodb-shell/reference/methods/
 
 db = new Mongo().getDB("subaybay");
-db.createCollection("cats")
+db.createCollection("cats");
+db.createCollection("requestTypes");
 
 db.cats.insertMany([
   {
@@ -15,3 +16,35 @@ db.cats.insertMany([
   }
 ]);
 
+db.users.insertMany([
+  {
+    name: "Yuumi",
+    email: "yuumi@lol.com",
+    profileUrl: "https://www.mobafire.com/images/champion/square/yuumi.png",
+    isAdmin: false
+  },
+  {
+    name: "Smolder",
+    email: "smolder@lol.com",
+    profileUrl: "https://www.mobafire.com/images/champion/square/smolder.png",
+    isAdmin: false
+  },
+  {
+    name: "Gnar",
+    email: "gnar@lol.com",
+    profileUrl: "https://www.mobafire.com/images/champion/square/gnar.png",
+    isAdmin: false
+  },
+  {
+    name: "Aurelion Sol",
+    email: "sol@lol.com",
+    profileUrl: "https://www.mobafire.com/images/champion/square/aurelion-sol.png",
+    isAdmin: false
+  },
+  {
+    name: "Kog' Maw",
+    email: "kogmaw@lol.com",
+    profileUrl: "https://www.mobafire.com/images/champion/square/kogmaw.png",
+    isAdmin: false
+  }
+])
