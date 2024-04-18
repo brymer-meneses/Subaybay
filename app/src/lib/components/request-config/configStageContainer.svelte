64cd8ac114cd730940da9558e0c6182d8da029ae
arktii <72131971+Arktii@users.noreply.github.com>
2024-04-18 12:35:11 +0800

    Improve configuration ui

--- app/src/lib/components/request-config/configStageContainer.svelte
+++ app/src/lib/components/request-config/configStageContainer.svelte
@@ -18,7 +18,7 @@
 
   export let users: UserData[];
 
-  export let onClick = () => {};
+  export let deleteStageFunction = (stageIndex : number) => {};
 
   let substages = stages[stageIndex];
 
@@ -38,9 +38,9 @@
   }
 </script>
 
-<Card class="flex-grow border-gray-400">
+<Card class="flex-grow border-gray-300">
   <CardHeader class="border-black">
-    <CardTitle>Stage {stageIndex + 1}</CardTitle>
+    <CardTitle class="col-span-*">Stage {stageIndex + 1}</CardTitle>
     {#if stageIndex == 0}
       <CardDescription>This is the request creation stage, it cannot be removed.</CardDescription>
     {/if}
@@ -66,7 +66,7 @@
               isDeletable={true}
               substageIndex={substageIndex}
               handlerIndex={substageData.handlerIndex}
-              deleteFunction={() => deleteSubstage(substageIndex)}
+              deleteFunction={deleteSubstage}
               onHandlerEdited={editHandler}
               {users}
             />
@@ -75,7 +75,7 @@
       {/if}
 
       {#if stageIndex > 0}
-        <!--Add Button-->
+        <!--Button for adding new substage-->
         <Button class="col-span-8 col-start-2" variant="outline" on:click={addSubstage}>
           <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
         </Button>   
@@ -83,8 +83,9 @@
     </div>    
   </CardContent>
   {#if stageIndex > 0}
+    <!--Button for deleting stage-->
     <CardFooter class="flex justify-between">
-      <Button class="bg-pale-red-100" variant="outline" on:click={onClick}>Delete</Button>
+      <Button variant="outline" on:click={() => deleteStageFunction(stageIndex)}>Delete</Button>
     </CardFooter>
   {/if}
 </Card>
--- app/src/lib/components/request-config/requestConfig.svelte
+++ app/src/lib/components/request-config/requestConfig.svelte
@@ -3,10 +3,12 @@
   import { UserData, SubstageData } from "./configConstants";
   import ConfigStageContainer from "./configStageContainer.svelte";
   import defaultProfilePic from "$lib/assets/defaultProfilePic.jpg";
+  import Input from "../ui/input/input.svelte";
 
   let stages: SubstageData[][] = [];
+  let requestType: string;
 
-  //hardcoded dummy data - to be replaced
+  //hardcoded dummy data - to be replaced by reading from database
   let users: UserData[] = [
     new UserData("-1", "None", defaultProfilePic),
     new UserData(
@@ -45,48 +47,42 @@
   }
 
   function storeInDatabase() {
-    //todo convert stages into a prompt on the database; all of the information is already there
+    //todo pass the data for validation to the server
+      //+ server will convert stages into a prompt on the database; all of the information is already there
   }
 </script>
 
-{#key stages.length}
-  <div
-    class="justify-center flex"
-    style="overflow-y: auto; height: 100%; padding-bottom: 200px;" 
-  > <!--todo make this use tailwind-->
-    <div class="grid grid-cols-1 gap-5" style="width:600px; margin-top: 10px; margin-bottom: 100px;"> <!--todo make this use tailwind-->
-      <input placeholder="Request Name" />
+<!--Todo? Use <Form>-->
+{#key stages}
+<div class="justify-center flex">
+  <div class="w-600 grid grid-cols-1 gap-5">
+    <Input bind:value={requestType} class="focus-visible:ring-0" placeholder="Request Type (e.g. OTR-1)"/>
+    {#if stages.length > 0}
+      <ConfigStageContainer
+        stageIndex={0}
+        {stages}
+        {users}
+      />
+
       {#if stages.length > 0}
-        <ConfigStageContainer
-          stageIndex={0}
-          {stages}
-          {users}
-        />
+        {#each stages as _, stageIndex}
+          {#if stageIndex > 0}
+            <ConfigStageContainer
+              stageIndex={stageIndex}
+              deleteStageFunction={deleteStage}
+              {stages}
+              {users}
+            />
+          {/if}
+        {/each}
+      {/if}
 
-        {#if stages.length > 0}
-          {#each stages as _, stageIndex}
-            {#if stageIndex > 0}
-              <ConfigStageContainer
-                stageIndex={stageIndex}
-                onClick={() => deleteStage(stageIndex)}
-                {stages}
-                {users}
-              />
-            {/if}
-          {/each}
-        {/if}
+      {/if}
+    <Button class="border-gray-300" variant="outline" on:click={addStage}>Add New Stage</Button>
 
-        {/if}
-      
-      <div>
-        <Button class="color-pale-yellow" on:click={addStage}>Add New Stage</Button>
-      </div>
-      <div>
-        <Button>Submit</Button>
-      </div>
-
-      <div class="flex" style="height: 200px;"></div>
+    <div class="justify-center flex">
+      <Button>Create</Button>
     </div>
   </div>
+</div>
 {/key}
-
