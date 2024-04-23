f6dd469cdb446b6dc568771ba9a328278f6d9693
arktii <72131971+Arktii@users.noreply.github.com>
2024-04-23 21:20:15 +0800

    Start ProgressView

--- app/src/routes/(dashboard)/requests/[requestId]/+page.server.ts
+++ app/src/routes/(dashboard)/requests/[requestId]/+page.server.ts
@@ -1,11 +1,35 @@
 import type { PageServerLoad } from "./$types";
+import {
+  type RequestType,
+  type Request,
+  type User,
+  database,
+} from "$lib/server/database";
 
 export const load: PageServerLoad = async (event) => {
-  console.log(event.params.requestId)
+  const requestId = event.params.requestId;
 
-  //todo get Request with requestId
-  //todo get RequestType from Request
-  //todo send appropriate details to progress view
+  const storedData = await retrieveData(requestId);
 
-  return { userInfo: event.locals.user };
+  return { userInfo: event.locals.user, request: storedData.request, requestType: storedData.requestType, users: storedData.users };
+};
+
+const retrieveData = async (requestId: string) => {
+  const cursor = database.collection<User>("users").find();
+  const users: { [_id: string]: { name: string; profileUrl: string } } = {};
+  for await (const user of cursor) {
+    users[user._id] = { name: user.name, profileUrl: user.profileUrl };
+  }
+
+  const request = await database
+    .collection<Request>("requests")
+    .findOne({ _id: requestId });
+  if (!request) return { users: null, request: null, requestType: null };
+
+  const requestType = await database
+    .collection<RequestType>("requestTypes")
+    .findOne({ _id: request.requestType });
+  if (!requestType) return { users: null, request: request, requestType: null };
+
+  return { users, request, requestType };
 };
