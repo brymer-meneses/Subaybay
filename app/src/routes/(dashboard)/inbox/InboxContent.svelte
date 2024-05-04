6d7bf92f55040110af61096bad8bcfc7c3dd7e56
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-05-05 05:22:18 +0800

    change design of request details

--- app/src/routes/(dashboard)/inbox/InboxContent.svelte
+++ app/src/routes/(dashboard)/inbox/InboxContent.svelte
@@ -9,18 +9,19 @@
   import ChatArea from "../ChatArea.svelte";
 
   import * as Card from "$lib/components/ui/card/index.js";
+  import { Badge } from "$lib/components/ui/badge";
+  import type { Request } from "$lib/server/database";
+  import { Textarea } from "$lib/components/ui/textarea";
 
-  export let requests: {[key: string]: any};
+  export let requests: { [key: string]: Request };
   export let selectedStage: any;
+
+  $: info = requests[selectedStage.requestId];
 </script>
 
 {#if selectedStage}
-  <Card.Root
-    class="overflow-hidden"
-    data-x-chunk-name="dashboard-05-chunk-4"
-    data-x-chunk-description="An order details card with order details, shipping information, customer information and payment information."
-  >
-    <Card.Header class="bg-muted/50 flex flex-row items-start">
+  <Card.Root class="overflow-hidden">
+    <Card.Header class="flex flex-row items-start bg-muted/50">
       <div class="grid gap-0.5">
         <Card.Title class="group flex items-center gap-2 text-lg">
           {#if selectedStage}
@@ -30,7 +31,7 @@
         <Card.Description>#00001</Card.Description>
       </div>
 
-      <div class="ml-auto flex items-center gap-1">
+      <div class="ml-auto flex items-center gap-2">
         <Button size="sm" variant="outline" class="h-8 gap-1">
           <span class="lg:sr-only xl:not-sr-only xl:whitespace-nowrap">
             View Progress
@@ -40,45 +41,19 @@
     </Card.Header>
     <Card.Content class="p-6 text-sm">
       <div class="grid gap-3">
-        <div class="font-semibold">Request Details</div>
-        <ul class="grid gap-3">
-          <li class="flex items-center justify-between">
-            <span class="text-muted-foreground">
-              Date <span></span>
-            </span>
-            <span>{selectedStage.dateSent}</span>
-          </li>
-          <li class="flex items-center justify-between">
-            <span class="text-muted-foreground">
-              Student Name <span></span>
-            </span>
-            <span>{requests[selectedStage.requestId].studentName}</span>
-          </li>
-          <li class="flex items-center justify-between">
-            <span class="text-muted-foreground">
-              Email <span></span>
-            </span>
-            <span>{requests[selectedStage.requestId].studentEmail}</span>
-          </li>
-          <li class="flex items-center justify-between">
-            <span class="text-muted-foreground">
-              Student Number <span></span>
-            </span>
-            <span>{requests[selectedStage.requestId].studentNumber}</span>
-          </li>
-          <li class="flex items-center justify-between">
-            <span class="text-muted-foreground">
-              Purpose <span></span>
-            </span>
-            <span>{requests[selectedStage.requestId].purpose}</span>
-          </li>
-          <li class="flex items-center justify-between">
-            <span class="text-muted-foreground">
-              Remarks <span></span>
-            </span>
-            <span>{requests[selectedStage.requestId].remarks}</span>
-          </li>
-        </ul>
+        <p class="font-semibold">Request Details</p>
+        <div class="flex flex-row gap-2">
+          <Badge>{info.studentNumber}</Badge>
+          <Badge variant="secondary">{info.studentName}</Badge>
+          <Badge variant="secondary">{info.studentEmail}</Badge>
+        </div>
+
+        {#if info.purpose !== ""}
+          <Textarea disabled value={info.purpose} />
+        {/if}
+        {#if info.remarks !== ""}
+          <Textarea disabled value={info.remarks} />
+        {/if}
       </div>
       <div class="my-4 grid gap-3">
         <ChatArea roomId="abcd" />
