4b5e2ea31f42c5ffb1d32433884cad88039f670a
ZDSeven <zedrickmigueldeguzman@gmail.com>
2024-05-08 09:37:30 +0800

    updated layout draft

--- app/src/routes/(dashboard)/requests/+page.svelte
+++ app/src/routes/(dashboard)/requests/+page.svelte
@@ -2,24 +2,27 @@
   import * as Card from "$lib/components/ui/card/index.js";
   import * as Table from "$lib/components/ui/table";
   import ScrollArea from "$lib/components/ui/scroll-area/scroll-area.svelte";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { CircleChevronRight } from "lucide-svelte";
 
+  export let user: User[];
   //export let requests: OURRequest[];
   // For testing
   const requests = [
     {
       _id: "juststarted",
       requestTypeId: "coe",
-      studentNumber: "2020",
+      studentNumber: "2020-35221",
       studentName: "Hello World",
-      studentEmail: "student@student.com",
-      purpose: "Purpose",
-      remarks: "Good Morning",
+      studentEmail: "zddeguzman1@up.edu.ph",
+      purpose: "Lorem ipsum dolor sit amet, consectetur.",
+      remarks: "Lorem ipsum dolor sit amet, consectetur.",
     },
   ];
 </script>
 
 <main class="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-  <div class="grid flex-1 auto-rows-max items-start gap-4 pr-12">
+  <div class="grid flex-1 auto-rows-max items-start gap-4">
     <Card.Root>
       <Card.Header class="px-7">
         <Card.Title>Requests</Card.Title>
@@ -29,18 +32,31 @@
         <Table.Root>
           <Table.Header>
             <Table.Row class="grid w-full grid-cols-12 text-left">
-              <Table.Head class="col-span-1">ID</Table.Head>
-              <Table.Head class="col-span-1">Type</Table.Head>
-              <Table.Head class="col-span-2">Student Number</Table.Head>
-              <Table.Head class="col-span-2">Student Email</Table.Head>
-              <Table.Head class="col-span-3">Purpose</Table.Head>
-              <Table.Head class="col-span-3">Remarks</Table.Head>
+              <Table.Head class="col-span-1 grid items-center">ID</Table.Head>
+              <Table.Head class="col-span-1 grid items-center">Type</Table.Head>
+              <Table.Head class="col-span-2 grid items-center"
+                >Student Number</Table.Head
+              >
+              <Table.Head class="col-span-2 grid items-center"
+                >Student Email</Table.Head
+              >
+              <Table.Head class="col-span-3 grid items-center"
+                >Purpose</Table.Head
+              >
+              <Table.Head class="col-span-2 grid items-center"
+                >Remarks</Table.Head
+              >
+              <Table.Head class="col-span-1 hidden items-center"
+                >Actions</Table.Head
+              >
             </Table.Row>
           </Table.Header>
           <ScrollArea class="h-[28rem]">
             <Table.Body>
               {#each requests as request (request._id)}
-                <Table.Row class="grid w-full grid-cols-12 text-left">
+                <Table.Row
+                  class="grid w-full grid-cols-12 items-center text-left"
+                >
                   <Table.Cell class="col-span-1">{request._id}</Table.Cell>
                   <Table.Cell class="col-span-1"
                     >{request.requestTypeId}</Table.Cell
@@ -51,8 +67,22 @@
                   <Table.Cell class="col-span-2"
                     >{request.studentEmail}</Table.Cell
                   >
-                  <Table.Cell class="col-span-3">{request.purpose}</Table.Cell>
-                  <Table.Cell class="col-span-3">{request.remarks}</Table.Cell>
+                  <Table.Cell class="col-span-3"
+                    >{request.purpose.length > 35
+                      ? request.purpose.substring(0, 35) + "..."
+                      : request.purpose}</Table.Cell
+                  >
+                  <Table.Cell class="col-span-2"
+                    >{request.remarks.length > 35
+                      ? request.remarks.substring(0, 35) + "..."
+                      : request.remarks}</Table.Cell
+                  >
+                  <Table.Cell class="col-span-1">
+                    <!-- Button for individual request details -->
+                    <Button size="icon" variant="ghost"
+                      ><CircleChevronRight class="h-5 w-5" /></Button
+                    >
+                  </Table.Cell>
                 </Table.Row>
               {/each}
             </Table.Body>
