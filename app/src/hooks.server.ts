131c8ff585b6306a95433ea98e4ab4ae10007e26
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-05-23 07:38:08 +0800

    simplify build step
    Soon we will deploy the `app` image to docker hub. But the current setup
    will fail since we have a top-level await that gets ran at the build
    step (`await client.connect()`). This is problematic since we might not
    have a database instance during build step (because it happens through
    github actions).
    
    This commit fixes it, by putting `client.connect` on
    `src/hooks.server.ts`, and handling potential other issues. It also
    removes the need for passing the environment variables on the build
    image.

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -2,9 +2,17 @@
 //  https://lucia-auth.com/getting-started/sveltekit
 import { lucia } from "$lib/server/auth";
 import { type Handle } from "@sveltejs/kit";
-import { redirect, setFlash } from "sveltekit-flash-message/server";
+import { redirect } from "sveltekit-flash-message/server";
+import { client } from "$lib/server/database";
 
 export const handle: Handle = async ({ event, resolve }) => {
+
+  // NOTE: 
+  // Top level await on $lib/server/database runs the code on build-time somehow
+  // tis is bade since the database may not be started on build time. 
+  // This is a no-op, if the client is already connected
+  await client.connect();
+
   const sessionId = event.cookies.get(lucia.sessionCookieName);
   if (!sessionId) {
     event.locals.user = null;
