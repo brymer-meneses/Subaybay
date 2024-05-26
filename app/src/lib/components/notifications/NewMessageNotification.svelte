bea5897bc3ebbeb5c2f5b2f68fd1fea774ff7b08
ZDSeven <zedrickmigueldeguzman@gmail.com>
2024-05-26 21:08:28 +0800

    changed message notif layout

--- app/src/lib/components/notifications/NewMessageNotification.svelte
+++ app/src/lib/components/notifications/NewMessageNotification.svelte
@@ -3,7 +3,7 @@
   import * as Avatar from "$lib/components/ui/avatar/index";
   import { createEventDispatcher } from "svelte";
   import { Button } from "$lib/components/ui/button";
-  import X from "lucide-svelte/icons/x";
+  import { X } from "lucide-svelte";
 
   export let message: Message;
   export let request: Request;
@@ -13,7 +13,7 @@
 </script>
 
 <div
-  class="relative flex h-[50px] w-[370px] flex-row items-center justify-between gap-2 rounded-full p-3"
+  class="flex h-[50px] w-[370px] flex-row items-center justify-between gap-2 rounded-full p-3"
 >
   <div class="flex flex-row items-center gap-4">
     <Avatar.Root class="h-7 w-7">
@@ -21,15 +21,15 @@
       <Avatar.Fallback>CN</Avatar.Fallback>
     </Avatar.Root>
     <p class="text-md text-muted-foreground">
-      {from.name.split(" ")[0]} sent you a message
+      {from.name.split(" ")[0]} sent you a message.
     </p>
   </div>
 
   <Button
-    variant="secondary"
+    variant="ghost"
     on:click={() => dispatch("closeToast")}
-    class="absolute left-[-10px] top-[-10px] flex items-center justify-center rounded-full p-0 opacity-80"
+    class="flex items-center rounded-full"
   >
-    <X size={10} />
+    <X size={18} />
   </Button>
 </div>
