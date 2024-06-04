c4669deff4e38b3bd04295f44c6957c5ee699108
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2024-06-04 21:47:07 +0800

    forgot to add proxy

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,29 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig, loadEnv } from "vite";
-import path from 'path';
 
 export default defineConfig(({ command, mode }) => {
-  const root = path.resolve(process.cwd(), '..');
-  const env = loadEnv(mode, root, '');
+  const env = loadEnv(mode, process.cwd(), '')
 
+  const url = mode == "production" ? "backend:8080" : "localhost:8080";
   return {
-    server: { 
-      fs: { 
+    server: {
+      fs: {
         allow: [".."]
       },
       proxy: {
+        '/notifications/events': {
+          changeOrigin: true,
+          target: `http://${url}/notifications/events`,
+          rewrite: (path) => path.replace(/^\/notifications^\events/, '')
+        },
+
+        // websocket
+        '/socket/': {
+          changeOrigin: true,
+          target: `ws://${url}`,
+          rewrite: (path) => path.replace(/^\/socket/, '')
+        },
+
         "/docs": {
           target: env.DOCS_URL,
           changeOrigin: true,
