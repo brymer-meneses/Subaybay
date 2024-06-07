630e0959340c70576a9eabdcd107a33e638a05b9
embryzon <95128949+embryzon@users.noreply.github.com>
2024-06-07 21:46:26 +0800

    fix student name accepting " "

--- app/src/routes/(dashboard)/inbox/schema.ts
+++ app/src/routes/(dashboard)/inbox/schema.ts
@@ -4,7 +4,7 @@ export const formSchema = z.object({
   studentNumber: z.string().refine((value) => /^\d{4}-\d{5}$/.test(value), {
     message: "Invalid student number format must be in the form XXXX-XXXXX",
   }),
-  studentName: z.string().min(1, {
+  studentName: z.string().min(1).refine((value) => value.trim(), {
     message: "Must be a non-empty",
   }),
   studentEmail: z.string().email({ message: "Must be a valid email" }),
